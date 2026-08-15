ffd3b373b27a93bb33252f25b802f733a7f6c55b
Bojan Antonovic <bojan@antonovic.ch>
2026-08-15 17:52:54 +0200

    forgotten files

--- src/test/java/ch/antonovic/springcloudstream/SpringCloudStreamApplicationTests.java
+++ src/test/java/ch/antonovic/springcloudstream/SpringCloudStreamApplicationTests.java
@@ -2,8 +2,11 @@ package ch.antonovic.springcloudstream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.cloud.stream.default-binder=test")
+@Import(TestChannelBinderConfiguration.class)
 class SpringCloudStreamApplicationTests {
 
 	@Test
